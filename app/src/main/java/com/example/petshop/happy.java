fd039733b5617413829b58beddd0293ce65cfa64
MuhammadAnas <khananas1291@gmail.com>
2026-01-08 23:06:00 -0700

    Initial Commit: Added files successfully

---
+++ app/src/main/java/com/example/petshop/happy.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class happy extends Mood {
+
+    public happy() {
+        super();
+    }
+
+    public happy(Date date) {
+        super(date);
+    }
+
+    public String feeling() {
+        return "happy";
+    }
+}
---
+++ app/src/main/java/com/example/petshop/sad.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class sad extends Mood {
+
+    public sad() {
+        super();
+    }
+
+    public sad(Date date) {
+        super(date);
+    }
+
+    public String feeling() {
+        return "sad";
+    }
+}
